89dfb23324f1996ac295e1cd686a27824d540768
rafael-abuawad <rafael.abuawad@live.com>
2023-09-07 19:50:41 -0400

    Removed unnecesary JSON

--- app/types/auth.types.go
+++ app/types/auth.types.go
@@ -1,38 +1,38 @@
 package types
 
 type LoginDTO struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required"`
+	Email    string `validate:"required,email"`
+	Password string `validate:"required"`
 }
 
 type SignupDTO struct {
 	LoginDTO
-	Name            string `json:"name" validate:"required,min=3"`
-	PasswordConfirm string `json:"password_confirm" validate:"required,eqfield=Password"`
+	Name            string `validate:"required,min=3"`
+	PasswordConfirm string `validate:"required,eqfield=Password"`
 }
 
 type ForgotPasswordDTO struct {
-	Email string `json:"email" validate:"required,email"`
+	Email string `validate:"required,email"`
 }
 
 type PasswordResetDTO struct {
-	Password        string `json:"password" validate:"required"`
-	PasswordConfirm string `json:"password_confirm" validate:"required,eqfield=Password"`
+	Password        string `validate:"required"`
+	PasswordConfirm string `validate:"required,eqfield=Password"`
 }
 
 type UserResponse struct {
-	ID         uint   `json:"id"`
-	Name       string `json:"name"`
-	Email      string `json:"email"`
-	Password   string `json:"-"`
-	ResetToken string `json:"-"`
+	ID         uint
+	Name       string
+	Email      string
+	Password   string
+	ResetToken string
 }
 
 type AccessResponse struct {
-	Token string `json:"token"`
+	Token string
 }
 
 type AuthResponse struct {
-	User *UserResponse   `json:"user"`
-	Auth *AccessResponse `json:"auth"`
+	User *UserResponse
+	Auth *AccessResponse
 }
